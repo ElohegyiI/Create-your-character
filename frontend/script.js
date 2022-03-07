6e4a3de327c03f4a6e340edf14f700623cc9cf73
ElohegyiI <roxnadz78@gmail.com>
2022-03-07 16:11:40 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -25,61 +25,65 @@ function loadEvent() {
     const charSheet = document.createElement('div')
         charSheet.className = 'charactersheet'
         document.getElementById('root').appendChild(sect).appendChild(charSheet)
-
-
-        
+    
         
         
         divisionChar.insertAdjacentHTML("beforeend", ` 
         <form>
-            <label for="charname">Character name:</label><br>
-            <input type="text" id="charname" name="charname" placeholder="Enter your charactername...">
+        <label for="charname">Character name:</label><br>
+        <input type="text" id="charname" name="charname" placeholder="Enter your charactername..."/>
+        <p id="values"></p>
         </form>`
         );
+
         
-        divisionClass.insertAdjacentHTML("beforeend", `<label                   for="class">Classes:</label>
-        <select name="class" id="class">
-        <optgroup>
-            <option value="Bard">Bard</option>
-            <option value="Rogue">Rogue</option>
-            <option value="Sorcerer">Sorcerer</option>
-        </optgroup>
-        </select>`
+        divisionClass.insertAdjacentHTML("beforeend", `
+        <label for="class">Classes:</label>
+            <select name="class" id="class">
+                <optgroup label='Pick a class'>
+                    
+                    <option value="Bard">Bard</option>
+                    <option value="Rogue">Rogue</option>
+                    <option value="Sorcerer">Sorcerer</option>
+                </optgroup>
+            </select>`
         );
         
         divisionRaces.insertAdjacentHTML("beforeend", `<label for="race">Races:</  label>
-        <select name="race" id="race">   
-        <optgroup>
-            <option value="Bugbear">Bugbear</option>
-            <option value="Deep Gnome">Deep Gnome</option>
-            <option value="Dragon Born">Dragon Born (Gem)</option>
-        </optgroup>
-        </select>`
+            <select name="race" id="race">   
+                <optgroup label='Pick a race'>
+                    <option value="Bugbear">Bugbear</option>
+                    <option value="Deep Gnome">Deep Gnome</option>
+                    <option value="Dragon Born">Dragon Born (Gem)</option>
+                </optgroup>
+            </select>`
         );
         
         divisionGender.insertAdjacentHTML("beforeend", `<label          for="gender">Gender:</label>
             <select name="gender" id="gender">   
-            <optgroup>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </optgroup>
+                <optgroup label='Pick a gender'>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </optgroup>
             </select>`
         );
         
-        const input = document.querySelector('charactername');
+        
+        const input = document.querySelector('input');
+        
         const log = document.getElementById('values');
             
             input.addEventListener('input', updateValue);
             
         function updateValue(e) {
           log.textContent = e.target.value;
-          console.log(updateValue(e))
+          //console.log(updateValue(e))
         }
         
         charSheet.insertAdjacentHTML("beforeend", `
-        <p id="values"></p>`
+        <p>${log}</p>`
         );
         
-
-}
-window.addEventListener("load", loadEvent)
+        
+    }
+    window.addEventListener("load", loadEvent)
